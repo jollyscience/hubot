b106b4c1d698bd12a23fd0e8fb9e0ff4612feda2
Meshach Jackson <meshach@jollyscience.com>
2013-06-14 19:50:52 -0400

    debugging in run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 export HUBOT_HIPCHAT_JID="35604_343802@chat.hipchat.com"
 export HUBOT_HIPCHAT_PASSWORD="J05i-hubot"
 export HUBOT_AUTH_ADMIN="245953,245954,246044"
-#export HUBOT_LOG_LEVEL="debug"
+export HUBOT_LOG_LEVEL="debug"
 export PORT="808080"
 export HUBOT_HARVEST_SUBDOMAIN="jollyscience"
 
