a6f8c91e23341c30158476da3d736cfd30eefe27
root <root@jsdev.jollyscience.com>
2013-06-22 20:41:00 -0400

    use mongo brain from https://raw.github.com/shanestillwell/hubot-mongo-brain/master/mongo-brain.coffee

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 export HUBOT_HIPCHAT_JID="35604_343802@chat.hipchat.com"
 export HUBOT_HIPCHAT_PASSWORD="J05i-hubot"
 export HUBOT_AUTH_ADMIN="245953,245954,246044"
-export HUBOT_LOG_LEVEL="debug"
+#export HUBOT_LOG_LEVEL="debug"
 export PORT="808080"
 export HUBOT_HARVEST_SUBDOMAIN="jollyscience"
-#export MONGOLAB_URI=""
+export MONGOLAB_URI="mongodb://josi:Wresp2T2@localhost:27017"
 
 bin/hubot --adapter hipchat
 #bin/hubot
