08b67c836d7e7b3a3e6d1e1461593657d8004169
Meshach Jackson <meshachjackson@gmail.com>
2013-08-04 18:57:13 -0500

    fixing errors in run script

--- _run.sh
+++ _run.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-<<<<<<< HEAD
 export HUBOT_LOG_LEVEL="debug"
 export PORT="808080"
-=======
 
 export HUBOT_HIPCHAT_JID="35604_343802@chat.hipchat.com"
 export HUBOT_HIPCHAT_PASSWORD="J05i-hubot"
 export HUBOT_AUTH_ADMIN="245953,245954,246044"
-#export HUBOT_LOG_LEVEL="debug"
-export PORT="808080"
 process.env['VARIABLE'] = 'value';
 
 #bin/hubot --adapter hipchat
->>>>>>> 9e75f8b647d7192475398da8b3a6aad0e5ba8d27
 bin/hubot
